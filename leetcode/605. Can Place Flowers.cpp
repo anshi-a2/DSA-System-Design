0abd983365deaae256b5606f750d1044aa4beca4
Anshi Aggarwal <52320166+anshi-a2@users.noreply.github.com>
2026-02-18 21:02:48 +0530

    Create 605. Can Place Flowers.cpp

---
+++ leetcode/605. Can Place Flowers.cpp
@@ -0,0 +1,34 @@
+// PROBLEM :
+
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+  
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+
+// SOLUTION :
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        for (int i = 0; i < flowerbed.size(); i++) {
+            bool left = i == 0 || flowerbed[i - 1] == 0;
+            bool right = i == flowerbed.size() - 1 || flowerbed[i + 1] == 0;
+            
+            if (left && right && flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;        
+    }
+};
